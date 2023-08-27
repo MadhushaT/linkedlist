3c77eedf5e26fb5a89a496e058483d9e27b204ba
Madhusha <madhushasampath@gmail.com>
2023-08-28 00:08:55 +0530

    push value

--- index.js
+++ index.js
@@ -10,4 +10,22 @@ class LinkedList {
     this.head = null;
     this.size = 0;
   }
+
+  // // Push ------------------------------------------------------------
+  // -----------------------------------------------------------------
+
+  push(value) {
+    const newNode = new Node(value);
+
+    if (this.head === null) {
+      this.head = newNode;
+    } else {
+      let current = this.head;
+      while (current.next !== null) {
+        current = current.next;
+      }
+      current.next = newNode;
+    }
+    this.size++;
+  }
 }
